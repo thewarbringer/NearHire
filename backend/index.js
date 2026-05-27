66f2bc2added2353ce787b98e084e2eefebc08a6
sumit <golasumit55@gmail.com>
2026-05-27 23:41:23 +0530

    muj per nhi ho rha

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', require('./routes/auth'))
 
 // MongoDB Connection
 const connectDB = async () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
               }
             />
             <Route
-              path="/LoginUser"
+              path="/signinUser"
               element={
                 <LoginUser />
               }
