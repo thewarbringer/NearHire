481bac0cef0f6b1a7075dfc967c5f3da96ddcbc7
sumit <golasumit55@gmail.com>
2026-05-27 21:53:59 +0530

    nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/Main'
 import Register from './pages/Register'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
